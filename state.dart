ce4db0639fcc95fb0647584ccf558c1b64cefb2c
Truong Huynh Gia Bao <thgbao@vinamilk.com.vn>
2023-04-27 18:14:55 +0700

    update state

--- state.dart
+++ state.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:vinamilk_b2b/util/global_key_util.dart';
+import 'package:vinamilk_b2b/global/localization.dart';
 
 extension StateEx on State {
-  AppLocalizations get locale =>
-      AppLocalizations.of(GlobalKeyUtil.navigatorKey.currentContext ?? context);
+  AppLocalizations get locale => Localization().locale;
 }
