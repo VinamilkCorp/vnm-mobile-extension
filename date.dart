804ef8fcc6d20fa1d0a6424daf17e9eb44ead24a
Truong Huynh Gia Bao <thgbao@vinamilk.com.vn>
2023-05-18 15:20:45 +0700

    add ddMMyyy2

--- date.dart
+++ date.dart
@@ -2,4 +2,6 @@ import 'package:intl/intl.dart';
 
 extension DateTimeEx on DateTime {
   String get ddMMyyyy => DateFormat("dd/MM/yyyy").format(this);
+
+  String get ddMMyyyy2 => DateFormat("dd-MM-yyyy").format(this);
 }
